f5c9a63a4b8b4033aa7a90aee2ea4aad442bfa45
Piotr Bątor <pbator94@gmail.com>
2021-03-18 16:48:28 +0100

    Make scroll to top arrow show/hide on given scrollY

--- src/components/Display/SlideToTop.js
+++ src/components/Display/SlideToTop.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import * as FaIcons from "react-icons/fa";
 
 function SlideToTop() {
+  //add event listener to get scroll value
+  const [isVisible, setIsVisible] = useState(false);
+  let currentScroll = 0;
+  window.addEventListener("scroll", (e) => {
+    currentScroll = window.scrollY;
+    currentScroll > 200 ? setIsVisible(true) : setIsVisible(false);
+  });
 
   return (
-    <Link smooth={true} to="top" className={`mobile-arrow-to-top`}>
+    <Link
+      smooth={true}
+      to="top"
+      className={`mobile-arrow-to-top ${isVisible ? "" : "is-hidden"}`}
+    >
       <FaIcons.FaArrowAltCircleUp className={`arrow-to-top-icon`} />
     </Link>
   );
