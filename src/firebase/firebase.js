f31c1dd0705b2e3db3f5f0b92b267ea3a397c15a
Piotr Bątor <pbator94@gmail.com>
2021-04-03 11:58:36 +0200

    Extract firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBLo9BJ6TGRqwJLDbZyNPWFfk-nW4X7_LY",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "carwallet-react.firebaseapp.com",
     projectId: "carwallet-react",
     storageBucket: "carwallet-react.appspot.com",
-    messagingSenderId: "298613705409",
-    appId: "1:298613705409:web:af0a3c5b1d8f19e1a2e240"
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig);
